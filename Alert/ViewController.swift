37a9ea857e952c7250561fdc103d834123896562
Hanis <hanishassim@users.noreply.github.com>
2024-09-05 15:41:53 +0800

    Project init

---
+++ Alert/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Alert
+//
+//  Created by Hassim, Muhammad Hanis on 05/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet private(set) var button: UIButton!
+    
+    @IBAction private func buttonTap() {
+        let alert = UIAlertController(title: "Do the thing?",
+                                      message: "Let us know if you want to do the thing.",
+                                      preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default) { _ in
+            print(">> Cancel")
+        }
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            print(">> OK")
+        }
+        alert.addAction(cancelAction)
+        alert.addAction(okAction)
+        alert.preferredAction = okAction
+        present(alert, animated: true)
+    }
+    
+}
+
